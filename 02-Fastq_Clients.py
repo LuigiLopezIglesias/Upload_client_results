e9b22dceda620e66cf62d3612a416d02f3fbf426
LuigiLopezIglesias <yamishakka@gmail.com>
2018-08-01 17:17:53 +0200

    eliminado un print

--- 02-Fastq_Clients.py
+++ 02-Fastq_Clients.py
@@ -31,7 +31,6 @@ result_set = db.execute('select rs.sample_id, m.c_muestra_wineseq, tm.d_tipo_mue
 
 # Move and rename Fastq Files
 for i in result_set:
-  print(i)
   DbName = i[1]
   SampleType = i[2]
   RunName = i[3]
