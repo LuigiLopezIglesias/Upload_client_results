38ffaec8b24a516cbeb535fd37ca07c9ca9b704d
LuigiLopezIglesias <yamishakka@gmail.com>
2018-09-11 17:15:42 +0200

    All queries ready to make its function

--- BMK/Queries.py
+++ BMK/Queries.py
@@ -20,8 +20,8 @@ def DBconexion():
   return sa.create_engine("postgresql://"+os.environ['DB_USER']+":"+os.environ['DB_PASSWORD']+"@"+os.environ['DB_HOST']+"/"+os.environ['DB_NAME'])
 
 #--# Query against Database for Fastq files information-----------------------------------------------
-def DBFastqQuery(PoolName):
-  return DBconexion().execute('select rs.sample_id, m.c_muestra_wineseq, tm.d_tipo_muestra, rs.name, r.name, rs.chain_type_id '
+def DBWsFastqQuery(PoolName):
+  return pd.read_sql_query('select rs.sample_id, m.c_muestra_wineseq, tm.d_tipo_muestra, rs.name, r.name, rs.chain_type_id '
                               'from  run_samples rs'
                               ' join runs          r on rs.run_id         = r.id'
                               ' join muestra       m on m.id              = rs.sample_id'
@@ -30,26 +30,13 @@ def DBFastqQuery(PoolName):
                               'where r.name = \''+PoolName+'\''
                               ' and processed'
                               ' and processed_history = 1'
+                              ' and m.c_tipo_muestra in (1,2,3)'
                               ' and m.c_estado = 3 '
-                              'order by m.c_muestra_wineseq;')
+                              'order by m.c_muestra_wineseq;', DBconexion())
 
-#--# Query against Database for metadata information--------------------------------------------------
-def DBMetadataQuery(PoolName):
-  return DBconexion().execute('select distinct tm.d_tipo_muestra, c.ftp_path, m.id_cliente '
-                              'from  run_samples rs'
-                              ' join runs          r on rs.run_id         = r.id'
-                              ' join muestra       m on m.id              = rs.sample_id'
-                              ' join tipo_muestra tm on tm.c_tipo_muestra = m.c_tipo_muestra'
-                              ' join cliente       c on c.id              = m. id_cliente '
-                              'where r.name = \''+PoolName+'\''
-                              ' and processed_history = 1'
-                              ' and m.c_tipo_muestra != 1 and m.c_tipo_muestra != 2 and m.c_tipo_muestra != 3'
-                              ' and m.c_estado = 3 '
-                              ' order by m.id_cliente ')
-
-#--# Query Against Database for Abundances origin-----------------------------------------------------
+#--# Query Against Database for BMK Abundances origin-----------------------------------------------------
 def DBAbundancesQuery(PoolName):
-  return pd.read_sql_query('select rs.sample_id, m.c_muestra_wineseq, tm.d_tipo_muestra, rs.name, r.name, c.ftp_path, ct.name'
+  return pd.read_sql_query('select rs.sample_id, m.c_muestra_wineseq, tm.d_tipo_muestra, rs.name, r.name, ct.name, c.ftp_path'
                               ' from run_samples   rs'
                               ' join runs           r on rs.run_id = r.id'
                               ' join muestra        m on m.id = rs.sample_id'
@@ -67,11 +54,24 @@ def DBAbundancesQuery(PoolName):
                                                             ' and m.c_tipo_muestra != 1 and m.c_tipo_muestra != 2 and m.c_tipo_muestra != 3'
                                                             ' order by rs.sample_id)'
                               ' and processed and processed_history = 1'
-                              #' and c.ftp_path != \'JacksonFamWine\''
                               ' and c.ftp_path != \'upload_errors\''
                               ' and r.name !=  \'upload_errors\'',DBconexion())
 
-#--# @uery to know finished samples for client and sampletype----------------------------------------- 
+#--# Query against Database for metadata information--------------------------------------------------
+def DBMetadataQuery(PoolName):
+  return DBconexion().execute('select distinct tm.d_tipo_muestra, c.ftp_path, m.id_cliente '
+                              'from  run_samples rs'
+                              ' join runs          r on rs.run_id         = r.id'
+                              ' join muestra       m on m.id              = rs.sample_id'
+                              ' join tipo_muestra tm on tm.c_tipo_muestra = m.c_tipo_muestra'
+                              ' join cliente       c on c.id              = m. id_cliente '
+                              'where r.name = \''+PoolName+'\''
+                              ' and processed_history = 1'
+                              ' and m.c_tipo_muestra != 1 and m.c_tipo_muestra != 2 and m.c_tipo_muestra != 3'
+                              ' and m.c_estado = 3 '
+                              ' order by m.id_cliente ')
+
+#--# Query to know finished samples for client and sampletype----------------------------------------- 
 def DBClientQuery(client, SampleType):
   return pd.read_sql_query('select m.c_muestra_wineseq, m.client_reference '
                                 'from muestra m'
